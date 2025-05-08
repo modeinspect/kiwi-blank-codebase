91d28687ff2bd85dc054097006744f37e95992ce
Peter Bokor <petttr1@gmail.com>
2025-05-08 22:00:22 +0200

    remove everything from index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Box from '@kiwicom/orbit-components/lib/Box';
-import {Illustration} from "@kiwicom/orbit-components";
-
 export default function Orbit() {
   return (
-    <Box padding="large"></Box>
+    <div></div>
   );
 }
